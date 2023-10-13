699aaca01328f482ee24e7d29ed0feb5e7903607
chaher13 <chahercrbs@gmail.com>
2023-10-13 22:00:15 +0200

    the coat

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,16 +1,16 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, "utf8");
-    const lines = data.split("\n").filter((line) => line.trim() !== "");
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
     let index = 1; // Commencez à partir de la deuxième ligne (l'indice 1)
     const totalStudents = lines.length - 1; // Soustrayez 1 pour exclure l'en-tête
     const studentsByField = {};
 
     while (index < lines.length) {
-      const [firstname, , , field] = lines[index].split(",");
+      const [firstname, , , field] = lines[index].split(',');
 
       if (field in studentsByField) {
         studentsByField[field].count += 1;
@@ -29,15 +29,11 @@ function countStudents(path) {
 
     for (const field in studentsByField) {
       if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
-        console.log(
-          `Number of students in ${field}: ${
-            studentsByField[field].count
-          }. List: ${studentsByField[field].list.join(", ")}`
-        );
+        console.log(`Number of students in ${field}: ${studentsByField[field].count}. List: ${studentsByField[field].list.join(', ')}`);
       }
     }
   } catch (error) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 }
 
