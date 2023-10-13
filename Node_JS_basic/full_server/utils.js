b2b2df29c0609c2b760a21f6faa68e14d3c91937
chaher13 <chahercrbs@gmail.com>
2023-10-13 22:21:26 +0200

    coat 8

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,20 +1,20 @@
 // full_server/utils.js
 
-const fs = require("fs");
+const fs = require('fs');
 
 function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, "utf8", (err, data) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
         reject(Error(err));
       } else {
-        const lines = data.trim().split("\n");
+        const lines = data.trim().split('\n');
         lines.shift(); // Supprimer l'en-tête
 
         const studentsByField = {};
 
         for (const line of lines) {
-          const [firstname, , , field] = line.split(",");
+          const [firstname, , , field] = line.split(',');
 
           if (!studentsByField[field]) {
             studentsByField[field] = [firstname];
